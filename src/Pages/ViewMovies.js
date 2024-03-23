80f73ca363de71d383d375ab9d8daaa1a01b80fe
prajwal <prajwal@prajwals-MacBook-Air.local>
2024-03-23 22:07:56 +0545

    kinks

--- src/Pages/ViewMovies.js
+++ src/Pages/ViewMovies.js
@@ -1,73 +1,80 @@
 // Import necessary libraries
 import React from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import NavBar from "../components/Navbar";
 
 
 const ViewMovies = () => {
   // Extract the id and image from the location state
   const { id, image, title, desc, lang, type, date } = useLocation().state;
-
+const navigate=useNavigate()
   // Construct the URL with the extracted id
   const url = `https://vidsrc.to/embed/movie/${id}`;
   const url2=`https://vidsrc.xyz/embed/movie/${id}`
   const time = date.slice(0, 4);
 
+ 
   return (
     <>
       <NavBar />
-      <section className="container">
-        <div className="row pt-3 d-flex align-items-center">
-          <div className="py-4 text-center note">
-            Note : You can change the server from the player located at top left
-            of the player.
-          </div>
-          <div className="col-lg-9">
-            <div>
-            <iframe
-            className="vid-player"
-            src={url2}
-            title={`Movie ${id}`}
-            width="100%"
-            height="500px"
-            allowFullScreen="true" 
-            webkitallowfullscreen="true"
-            mozallowfullscreen="true"
-            scrolling="no"
-            frameborder="0"
-          ></iframe>
-          
-            </div>
-          </div>
+     <section className="container">
 
-          <div className="col-lg-3">
-            <div className="text-center">
-              <img
-                src={`https://image.tmdb.org/t/p/w500${image}`}
-                alt="Movie Poster"
-                width="300px"
-                className="poster"
-               
-              />
-            </div>
-          </div>
+     <div>
+     <img src="https://swipefile.com/wp-content/uploads/2022/05/cta-banner-offer-copy.png" 
+     alt="" 
+     width="100%" 
+     height="200px"/>
+     </div>
+     <div className="row py-4">
+     <div className="col-lg-6">
+     <div className="d-flex gap-3 ps-3">
+    
+     <div className="link-1 ">
+     <h5 className="fw-bold">Server 1</h5>
+    <div className="py-3">
+    Vidsrc 1
+    </div>
+    <a href={`https://vidsrc.to/embed/movie/${id}`} className="movie-links ">
+    ClickHere
+    </a>
+     </div>
+   
+     <div className="link-1">
+     <h5 className="fw-bold">Server 2</h5>
+    <div className="py-3">
+    
+    Vidsrc 2
+  
+   
+    </div>
+    <a href={`https://vidsrc.to/embed/movie/${id}`} className="movie-links ">
+    ClickHere
+    </a>
+  
+     
+     </div>
 
-          <div className="col-lg-5">
-            <h5 className="py-3">
-              {title} ({time})
-            </h5>
-            <div className="pb-2">Language: {lang}</div>
-            {type ? (
-              <>
-                <span className="sm-text">Type: {type}</span>
-              </>
-            ) : (
-              <></>
-            )}
-            <div>Description: {desc}</div>
-          </div>
-        </div>
-      </section>
+     </div>
+     
+     </div>
+     <div className="col-lg-6"></div>
+     <div className="row py-5">
+     <div className="col-lg-3 text-center">
+     <img
+     src={`https://image.tmdb.org/t/p/w500${image}`}
+     alt="Movie Poster"
+     width="300px"
+     className="poster"
+   />
+     </div>
+     <div className="col-lg-4 py-4">
+    <h5 className="pb-2"> Name : {title} ({time}) </h5> 
+    <div className="pb-2"> language : {lang}</div>
+    <div> Description : {desc}</div>
+     </div>
+     </div>
+     </div>
+     </section>
     </>
   );
 };
