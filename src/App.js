1935966e3cee86cc80ab4b2fd98abd3727861f4b
prajwal000 <101589134+prajwal000@users.noreply.github.com>
2024-04-22 20:20:15 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,17 +13,6 @@ function App() {
     <>
    
       <CurrentPageProvider>
-   
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-4F9HXNWT02"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-4F9HXNWT02');
-</script>
-
         <Router />
       </CurrentPageProvider>
     </>
