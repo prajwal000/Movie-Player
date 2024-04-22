3f8a8b0b0645a192205a596fb8a2db6df0ff2840
prajwal <prajwal@prajwals-MacBook-Air.local>
2024-04-22 20:43:31 +0545

    analytics

--- src/index.js
+++ src/index.js
@@ -3,5 +3,13 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 
+import  ReactGA from "react-ga4";
+ReactGA.initialize("G-4F9HXNWT02");
+
+ReactGA.send({
+    hittype:"pageview",
+    page:window.location.pathname,
+
+});
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
